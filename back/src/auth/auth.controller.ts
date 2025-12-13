0a4a6f3d57068d7b55bbe422504cc650dd67492f
lauramussa <lauramussa94@gmail.com>
2025-12-12 22:45:57 -0300

    se agrego domain a main

--- back/src/auth/auth.controller.ts
+++ back/src/auth/auth.controller.ts
@@ -72,6 +72,8 @@ export class AuthController {
       expires: new Date(0),
       path: '/',
       maxAge: 1000 * 60 * 60 * 24,
+      domain: '.koyeb.app' // A veces ayuda, a veces rompe.
+
     });
 
     return { message: 'Sesión cerrada exitosamente' };
